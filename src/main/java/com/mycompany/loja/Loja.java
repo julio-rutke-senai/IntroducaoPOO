57a42bdffe248f7f0d82abe482e5b3cc0dfda35b
Professor SESI/SENAI <Professor@SN-353296.edu.fiesc.com.br>
2026-02-12 19:31:29 -0300

    Projeto Inicial

---
+++ src/main/java/com/mycompany/loja/Loja.java
@@ -0,0 +1,12 @@
+package com.mycompany.loja;
+
+/**
+ *
+ * @author Professor
+ */
+public class Loja {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
